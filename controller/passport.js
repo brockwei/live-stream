9ba302b7dbeab243e9e1f246e8d513ea467635b6
Isaac Yuen <whyuenac@gmail.com>
2017-12-05 14:55:43 +0800

    google passport

--- controller/passport.js
+++ controller/passport.js
@@ -1,29 +1,66 @@
 //passport.js
 const passport = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config();
 
 module.exports = (app) => {
-    app.use(passport.initialize());
-    app.use(passport.session());
-  
-    passport.use(new FacebookStrategy({
-        clientID: process.env.FACEBOOK_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL,
-        profileFields: ['email','name']
-      },
-      function(accessToken, refreshToken, profile, cb) {
-        return cb(null,{profile:profile,accessToken:accessToken});
-      }
-    ));
-    /*Understanding serializeUser & deserializeUser:
-    https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
-    */
-    passport.serializeUser((user,done)=>{
-      done(null,user);
-    });
-    passport.deserializeUser((user,done)=>{
-      done(null,user);
-    });
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL,
+    profileFields: ['email', 'name']
+  },
+    function (accessToken, refreshToken, profile, cb) {
+      return cb(null, { profile: profile, accessToken: accessToken });
+    }
+  ));
+
+  passport.use(new GoogleStrategy({
+    // options for google strategy
+    clientID: process.env.GOOGLE_clientID,
+    clientSecret: process.env.GOOGLE_clientSecret,
+    callbackURL: 'http://localhost:8080/auth/google/redirect',
+    scope: 'user:email',
+
+  }, (accessToken, refreshToken, profile, done) => {
+    // check if user already exists in our own db
+    // User.findOrCreate(profile, done)
+    return done(null, { profile: profile, accessToken: accessToken });
+
+  })
+  );
+  passport.use('local-login', new LocalStrategy(
+    (email, password, done) => {
+      Model.User.findOne({
+        where: {
+          'email': email
+        }
+      }).then((user) => {
+        if (user == null) {
+          return done(null, false, { message: 'Incorrect credentials.' });
+        }
+
+        if (user.password === password) {
+          return done(null, user);
+        }
+
+        return done(null, false, { message: 'Incorrect credentials.' });
+      });
+    }
+  ));
+
+  /*Understanding serializeUser & deserializeUser:
+  https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
+  */
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
 };
--- router/router.js
+++ router/router.js
@@ -4,35 +4,70 @@ module.exports = (express) => {
 
     const router = express.Router();
 
-    function isLoggedIn(req,res,next){
-        if(req.isAuthenticated()){
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) {
             return next();
         }
-        res.redirect('/auth/facebook');
+        res.redirect('/');
     }
-    // Secret Path to be protected
-    router.get('/test',  isLoggedIn, (req, res) => {
-        res.sendFile(__dirname+ '/test.html');
-    });
-    router.get('/auth/facebook', passport.authenticate('facebook',{ 
-        scope: ['user_friends', 'manage_pages','email'] 
-    }));
-    // The Redirect URL route.
-    router.get('/auth/facebook/callback', passport.authenticate('facebook',{ 
-        failureRedirect:'/'
-    }),(req,res)=>{
-        //console.log(req.session);
-        res.redirect('/test');
-    });
-    router.get('/error', (req, res) => {
-        res.send('You are not logged in!');
-    });
     router.get('/', (req, res) => {
         res.sendFile(__dirname + '/login.html');
     });
-    router.get('/logout',(req,res)=>{
-        req.logout();
-        res.redirect("/")
+    // Secret Path to be protected
+    router.get('/test', isLoggedIn, (req, res) => {
+        res.sendFile(__dirname + '/test.html');
+    });
+    router.get('/auth/facebook', passport.authenticate('facebook', {
+        scope: ['user_friends', 'manage_pages', 'email']
+    }));
+    // The Redirect URL route.
+    router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+        failureRedirect: '/'
+    }), (req, res) => {
+        //console.log(req.session);
+        res.redirect('/test');
+    });
+    // auth with google+
+    router.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    // callback route for google to redirect to
+    // hand control to passport to use code to grab profile info
+    router.get('/auth/google/redirect', passport.authenticate('google', {
+        failureRedirect: '/'
+    }), (req, res) => {
+        res.redirect('/test');
+    });
+    router.get('/localLogin', (req, res) => {
+        res.sendFile(__dirname + '/localLogin.html');
+    });
+    router.get('/signup', (req, res) => {
+        res.sendFile(__dirname + '/signup.html');
+    });
+
+    // router.post('/signup', passport.authenticate('local-signup', {
+    //     successRedirect: '/',
+    //     failureRedirect: '/error'
+    // }));
+
+
+
+    router.get('/error', (req, res) => {
+        res.send('You are not logged in!');
+    });
+    // router.get('/logout', (req, res) => {
+    //     req.logout();
+    //     res.redirect("/")
+    // });
+
+    router.get('/logout', (req, res) => {
+
+        req.session.destroy((err) => {
+            console.log("logouted")
+            if (err) { console.log(err) }
+            res.redirect('/'); //Inside a callback… bulletproof!
+        })
     });
     return router;
 }
