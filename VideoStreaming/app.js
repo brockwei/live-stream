b7657b2924740577b43381a112be479fc7baa412
Michelle Bae <baehyunjai@gmail.com>
2017-12-06 14:43:50 +0800

    test

--- VideoStreaming/app.js
+++ VideoStreaming/app.js
@@ -2,7 +2,7 @@ const http = require ("http");
 
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n Hey this is Michelle here Brock and Isaac here too hehexd');
+        res.end('Hello World\n Hey this is Michelle here Brock and Isaac here too hehexd Hi we did it!!!');
     }).listen(8080,'localhost');
 
     console.log("Server running at http://localhost:8080/");
